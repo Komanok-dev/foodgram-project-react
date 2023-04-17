4738363fcef30a1c8099c102e1744f191f4f0b57
Игорь Терских <s.komanok@gmail.com>
2023-04-17 17:27:10 +0300

    Diplom

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 from users.models import Follow, User
 from users.serializers import FollowCreateSerializer, FollowListSerializer
 
